3b637cc38c50344da51c7d64df03324fca35da5f
lee_geunil <leegun1@gmail.com>
2016-02-21 17:28:42 +0900

    位置情報取得処理実装

--- CLLocationExsample/ViewController.swift
+++ CLLocationExsample/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var lm : CLLocationManager! = nil
+    var longitude : CLLocationDegrees!
+    var latitude : CLLocationDegrees!
+    
+    @IBOutlet weak var longitudeField: UITextField!
+    @IBOutlet weak var latitudeField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        longitude = 0.0
+        latitude = 0.0
+        longitudeField.text = String(longitude)
+        latitudeField.text = String(latitude)
+
+        lm = CLLocationManager()
+        lm.delegate = self
+        lm.requestAlwaysAuthorization()
+        lm.desiredAccuracy = kCLLocationAccuracyBest
+        lm.distanceFilter = 300
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onStartLocation(sender: AnyObject) {
+        lm.startUpdatingLocation()
+    }
+
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        longitude = newLocation.coordinate.longitude
+        latitude = newLocation.coordinate.latitude
+        longitudeField.text = String(longitude)
+        latitudeField.text = String(latitude)
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        NSLog("Error")
+    }
 
 }
 
